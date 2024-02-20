e510d6fedb640b49318a855669011920addf32d6
rizky.bagus <rizky.bagus@gits.id>
2024-02-20 10:25:25 +0700

    feat: collections test cases solution

---
+++ Collections/Collectionsdeque.py
@@ -0,0 +1,17 @@
+import collections
+
+n = int(input())
+d = collections.deque()
+for _ in range(n):
+    cmd = list(input().strip().split())
+    opt = cmd[0]
+    if opt == "append":
+        d.append(int(cmd[1]))
+    elif opt == "appendleft":
+        d.appendleft(int(cmd[1]))
+    elif opt == "pop":
+        d.pop()
+    elif opt == "popleft":
+        d.popleft()
+for i in d:
+    print(i, end=" ")
